a000569c4e801307e48f8c77b16473f6b9637fe5
danylollysychenko-ux <danylollysychenko@gmail.com>
2025-11-07 10:01:38 -0500

    Add files via upload

---
+++ length_of_last_word.py
@@ -0,0 +1,7 @@
+def length_of_last_word(s):
+    s = s.split()
+    print(len(s[-1]))
+
+length_of_last_word("Hello World")
+length_of_last_word(" fly me to the moon ")
+length_of_last_word("luffy is still joyboy")
